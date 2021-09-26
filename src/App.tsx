fd32a0f393f2ba4a91763d5954881b5e643267bc
Angelo de Salves <angelopeto@yahoo.com.br>
2021-09-26 18:15:33 -0300

    Module 1 - google fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import styled from 'styled-components'
 import { GlobalStyle } from "./styles/global";
 
-const Title = styled.h1`
-  color: #8257e6;
-  font-size: 64px;
-`
-
 export function App() {
   return (
     <div className="App">
       <GlobalStyle/>
-      <Title>Hello world</Title>
+      <h1>Hello world</h1>
     </div>
   );
 }

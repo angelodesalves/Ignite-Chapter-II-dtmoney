104d48c0c73353a7e18d65163bf9697f757cd82b
Angelo de Salves <angelopeto@yahoo.com.br>
2021-09-28 13:27:23 -0300

    Module 3 - fake API: miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,22 @@
+import { createServer } from 'miragejs';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'teste',
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>

2242167607107390ab561c602f60181590384f50
Angelo de Salves <angelopeto@yahoo.com.br>
2021-11-16 20:49:56 -0300

    Module 4 - modal style * form structure

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,6 @@
 import Modal from 'react-modal';
+import closeImg from '../../assets/close.svg';
+import { Container } from './styles';
 
 Modal.setAppElement('#root');
 
@@ -12,8 +14,30 @@ export function NewTransactionModal({ isOpen, onRequestClose} : NewTransactionMo
 		<Modal
 			isOpen={isOpen}
 			onRequestClose={onRequestClose}
+			overlayClassName="react-modal-overlay"
+			className="react-modal-content"
 		>
-			<h2>Cadastras transação</h2>
+			<button
+				type="button"
+				onClick={onRequestClose}
+				className="react-modal-close"
+			>
+				<img src={closeImg} alt="Fechar modal" />
+			</button>
+
+			<Container>
+				<h2>Cadastras transação</h2>
+
+				<input type="text" placeholder="Título" />
+
+				<input type="number" placeholder="Valor" />
+
+				<input type="text" placeholder="Categoria" />
+
+				<button type="submit">
+					Cadastrar
+				</button>
+			</Container>
 		</Modal>
 	);
 }

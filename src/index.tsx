7c9d02757d601b3f48e03783d1cc112f8602dbc7
Angelo de Salves <angelopeto@yahoo.com.br>
2021-11-18 22:06:24 -0300

    Module 4 - Saving data on Mirage * listing transactions * value formatting

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,48 @@
-import { createServer } from 'miragejs';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
-  routes() {
-    this.namespace = 'api';
+	models: {
+		transaction: Model
+	},
+	seeds(server) {
+		server.db.loadData({
+			transactions: [
+				{
+					id: 1,
+					title: 'Freela de website',
+					type: 'deposit',
+					amount: 5000,
+					category: 'Dev',
+					createdAt: '2021-02-12 09:00:00'
+				},
+				{
+					id: 2,
+					title: 'Financiamento',
+					amount: 2000,
+					type: 'withdraw',
+					category: 'Casa',
+					createdAt: '2021-02-27 08:00:00'
+				}
+			]
+		});
+	},
 
-    this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'teste',
-        }
-      ]
-    })
-  }
+	routes() {
+		this.namespace = 'api';
+
+		this.get('/transactions', () => {
+			return this.schema.all('transaction')
+		})
+
+		this.post('/transactions', (schema, request) => {
+			const data = JSON.parse(request.requestBody)
+
+			return schema.create('transaction', data)
+		})
+	}
 })
 
 ReactDOM.render(

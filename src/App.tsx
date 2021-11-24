a038343a88dee51f8b851bdb0e3abe1ae4e2ab68
Angelo de Salves <angelopeto@yahoo.com.br>
2021-11-23 22:26:49 -0300

    Module 5 - Creation of TransactionsContext

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Header } from './components/Header'
 import { NewTransactionModal } from './components/NewTransactionModal';
 
 import { GlobalStyle } from "./styles/global";
+import { TransactionsContext, TransactionsProvider } from './TransactionsContext';
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
@@ -17,7 +18,7 @@ export function App() {
 	}
 
   return (
-    <>
+    <TransactionsProvider>
       <Header
         onOpenNewTransactionModal={handleOpenNewTransactionModal}
       />
@@ -27,6 +28,6 @@ export function App() {
         onRequestClose={handleCloseNewTransactionModal}
       />
       <GlobalStyle/>
-    </>
+    </TransactionsProvider>
   );
 }
---
+++ src/TransactionsContext.tsx
@@ -0,0 +1,57 @@
+import { createContext, useEffect, useState, ReactNode } from 'react'
+import { api } from './services/api';
+
+interface Transaction {
+	id: number,
+	title: string,
+	amount: number,
+	type: string,
+	category: string,
+	createdAt: string
+}
+
+// interface TransactionInput {
+// 	title: string,
+// 	amount: number,
+// 	type: string,
+// 	category: string
+// }
+
+// type TransactionInput = Pick<Transaction, 'title' | 'amount' | 'type' | 'category'>
+
+type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>
+
+interface TransactionsProviderProps {
+	children: ReactNode
+}
+
+interface TransactionsContextData {
+	transactions: Transaction[];
+	createTransaction: (transaction: TransactionInput) => void;
+}
+
+export const TransactionsContext = createContext<TransactionsContextData>(
+//	ao criar um contexto, gera erro pois não retorna o formato esperado
+//	o recomendado é "forçar" a tipagem para enganar o typescript
+//	isso não acaba sendo um problema, pois o valor já é setado no provider logo abaixo
+	{} as TransactionsContextData
+);
+
+export function TransactionsProvider({ children } : TransactionsProviderProps) {
+	const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+	useEffect(() => {
+		api.get('transactions')
+			.then(response => setTransactions(response.data.transactions));
+	}, []);
+
+	function createTransaction(transaction: TransactionInput) {
+		api.post('/transactions', transaction);
+	}
+
+	return (
+		<TransactionsContext.Provider value={{ transactions, createTransaction }}>
+			{children}
+		</TransactionsContext.Provider>
+	)
+}
